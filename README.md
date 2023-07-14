04ab5cb55682b0cc26ef18eb670d6c26bc744673
JuanSanchezKZ <juanzete@gmail.com>
2023-07-14 07:50:17 -0300

    inicializacion de proyecto React

--- README.md
+++
@@ -1,2 +0,0 @@
-# aquario
-Proyecto fundacion pescar
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ComponenteEjemplo from './components/ComponenteEjemplo'
+
+const App = () => {
+  
+  return (
+    <div>
+      <ComponenteEjemplo/>
+      </div>
+  )
+}
+
+export default App
