7153926db66aa6106c3de77913777e4316b2a08c
JuanSanchezKZ <juanzete@gmail.com>
2023-07-21 18:25:53 -0300

    creacion de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,31 @@
 import './Login.css'
 import logo from '../../assets/LogoFundacionPescar.png'
+import axios from 'axios'
 
 const Login = () => {
+
+
+
+
+
 return (
     <form>
-        <div className="row">
       <img src={logo} alt="Logo Fundación Pescar" />
-      <p>Recuerda que tu usuario es el e-mail con el que te registraste en fundación pescar..</p>
-      <label htmlFor="">Usuario</label>
-      <input type="text" />
-      <label htmlFor="">Contraseña</label>
-      <input type="password" />
-      <div>
-        <input type="checkbox" />
-        <span>Recordar usuario</span>
-        <p>Olvidé mi contraseña</p>
+      <p className='mt-3'>Recuerda que tu usuario es el e-mail con el que te registraste en fundación pescar..</p>
+      <label className='mt-1' htmlFor="">Usuario</label>
+      <input className='mt-1'  type="text" name='user' />
+      <label className='mt-3' htmlFor="">Contraseña</label>
+      <input className='mt-1' type="password" name='password' />
+      <div className='bottom-inputs mt-3 mb-3'>
+        <div>
+         <input type="checkbox" />
+         <span>Recordar usuario</span>
+        </div>
+        <div>
+         <span>Olvidé mi contraseña</span>
+        </div>
       </div>
-      <button type="submit">Ingresar</button>
-      </div>
+      <button id='button'  type="submit">Ingresar</button>
     </form>
 )
 }
