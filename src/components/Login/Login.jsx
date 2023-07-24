4683bec47a3d653f772223509ec98e08705fed18
JuanSanchezKZ <juanzete@gmail.com>
2023-07-24 17:52:22 -0300

    Login finalizado

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,28 +6,27 @@ const Login = () => {
     event.preventDefault()
     const email = event.target.user.value
     const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g
+
     if (email !== "" && !regexEmail.test(email)) {
       return;
     }
+    
   }
   return (
     <form onSubmit = {formHandler}>
       <img src={logo} alt="Logo Fundación Pescar" />
-      <p className='mt-3'>Recuerda que tu usuario es el e-mail con el que te registraste en fundación pescar.</p>
+      <p className='text-center mt-3'>Recuerda que tu usuario es el e-mail con el que te registraste en fundación pescar.</p>
       <label className='mt-1' htmlFor="">Usuario</label>
       <input className='mt-1'  type="text" name='user' />
       <label className='mt-3' htmlFor="">Contraseña</label>
       <input className='mt-1' type="password" name='password' />
-      <div className='bottom-inputs mt-3 mb-3'>
-        <div>
-        <input type="checkbox" />
-        <span>Recordar usuario</span>
+      
+        <div className="checkbox mt-3">
+         <input type="checkbox" />
+         <span>Recordar usuario</span>
         </div>
-        <div>
-        <span class="underlined">¿Olvidaste tu contraseña?</span>
-        </div>
-      </div>
-    <button id='button'  type="submit">Ingresar</button>
+    <button className="mt-3" id='button'  type="submit">Ingresar</button>
+      <a  href='#'>¿Olvidaste tu contraseña?</a>
     </form>
   )
 }
