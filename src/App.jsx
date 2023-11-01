0f31d4f0ebe6bc0a6f43f6312f799fbad3108642
Valentina Milagros Castillo <valentinacastillo936@gmail.com>
2023-11-01 17:10:11 -0300

    Agrego la parte de participantes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Fondo from "./components/Login/Fondo/Fondo"
 import { Routes, Route} from "react-router-dom"
 import { Avisos } from './components/Avisos/Avisos'
 import {Perfil} from './components/Perfil/Perfil';
+import { Participantes } from './components/Participantes/Participantes';
+
 const App = () => {
   return (
     <div>
@@ -11,6 +13,7 @@ const App = () => {
         <Route path="/" element= { <Fondo/> }> </Route>    
         <Route path="/avisos" element= { <Avisos/> }> </Route>
         <Route path="/perfil" element= { <Perfil/> }> </Route>
+        <Route path="/participantes" element= { <Participantes/> }></Route>
         
                 
       </Routes>
