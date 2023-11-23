b3c20925b4e613ecf7c746ca8482304dfb4e75c9
Valentina Milagros Castillo <valentinacastillo936@gmail.com>
2023-11-23 19:14:04 -0300

    Participantes resposive

--- src/components/Participantes/Participantes.jsx
+++ src/components/Participantes/Participantes.jsx
@@ -6,19 +6,47 @@ import styles from './Participantes.module.css'
 export const Participantes = () => {
     return (
         <div>
-            <Header/>
-            <Navbar/>
+            <Header />
+            <Navbar />
+            <section className={styles.usuarios}>
+                <div className={styles.usuariosMobile}>
+                    <div className={styles.botones}>
+                        <button className={styles.botonAlumno}>Alumnos</button>
+                        <button className={styles.botonCoordinadora}>Coordinadoras</button>
+                    </div>
+                    <div className={styles.orden}>
+                        <div>
+                            <h3>Alumnos</h3>
+                            <p>Total: 20</p>
+                        </div>
+                        <div>
+                            <select className={styles.select} name="" id="">
+                                <option selected>Ordenar por</option>
+                                <option value="Alumno">Alumno</option>
+                                <option value="Coordinadora">Coordinadora</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className={styles.buscador}>
+                        <input className={styles.input} type="text" />
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                        </svg></span>
+                    </div>
+                </div>
+                <div className={styles.noVisible}>
+                    <p>Nombre</p>
+                    <p>Rol</p>
+                </div>
 
-            <section className= {styles.usuarios}>
-                <Usuario nombre="Juan" role="Alumno"/> {/* props paso datos a mi componente */}
-                <Usuario nombre="Ana" role="Alumno"/>
-                <Usuario nombre="Carlos" role="Alumno"/>
-                <Usuario nombre="Juan" role="Alumno"/>
-                <Usuario nombre="Ana" role="Alumno"/>
-                <Usuario nombre="Carlos" role="Alumno"/>
-                <Usuario nombre="María Sol" role="coordinadora"/>
-                <Usuario nombre="Lucía Flores" role="coordinadora"/>
-                
+                <Usuario nombre="Juan López" role="Alumno" /> 
+                <Usuario nombre="Ana" role="Alumno" />
+                <Usuario nombre="Carlos" role="Alumno" />
+                <Usuario nombre="Juan" role="Alumno" />
+                <Usuario nombre="Ana" role="Alumno" />
+                <Usuario nombre="Carlos" role="Alumno" />
+                <Usuario nombre="María Sol" role="coordinadora" />
+                <Usuario nombre="Lucía Flores" role="coordinadora" />
             </section>
         </div>
     )
--- src/components/Participantes/Usuario/Usuario.jsx
+++ src/components/Participantes/Usuario/Usuario.jsx
@@ -10,17 +10,15 @@ export const Usuario = (props) => {
                     <div className={styles.userPerson + " " + styles.coordinadora}>
                         <img className={styles.personImg} src="https://cdn.discordapp.com/attachments/1168750269383856150/1168797223509827625/avatar-de-usuario_3.png?ex=6553123d&is=65409d3d&hm=218aa407ee7888e3dd2a99991ad7953e61fd073361b293269d2cedb2bd1bcfb6&" alt="User" />
                         <h2>{props.nombre}</h2>
-                        <p>({props.role})</p>
-                        <p>Centro Pescar Santander</p>
-                        <img className={styles.socialImg} src="https://cdn-icons-png.flaticon.com/128/61/61109.png" alt="linkedin" />
+                        <p>{props.role}</p>
+                        <p className={styles.centro}>Centro Pescar Santander</p>
                     </div>
                     :
                     <div className={styles.userPerson}>
                         <img className={styles.personImg} src="https://cdn.discordapp.com/attachments/1168750269383856150/1168785723487428628/avatar-de-usuario_2.png?ex=65530787&is=65409287&hm=0952984a277b8935860ac454e616901e11754d01c7cda3ea9262305798bd94d1&" alt="User" />
                         <h2>{props.nombre}</h2>
-                        <p>({props.role})</p>
-                        <p>Centro Pescar Santander</p>
-                        <img className={styles.socialImg} src="https://cdn-icons-png.flaticon.com/128/61/61109.png" alt="linkedin" />
+                        <p>{props.role}</p>
+                        <p className={styles.centro}>Centro Pescar Santander</p>
                     </div>
             }
         </>
